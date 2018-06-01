337660b7b0ff8f411f651f5a9e4c94afd8f80c97
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2018-06-01 16:57:14 +0300

    simpler is_even

--- is_even.ml
+++ is_even.ml
@@ -1,10 +1,6 @@
-let rec is_even x =
-  if x = 0 then true else is_odd (x - 1)
-and is_odd x =
-  if x = 0 then false else is_even (x - 1)
+let rec is_even x = x mod 2 = 0
 
 let test = List.map ~f:is_even [0;1;2;3;4;5]
            = [true; false; true; false; true; false]
 let test = List.map ~f:is_odd [0;1;2;3;4;5]
            = [false; true; false; true; false; true]
-             
