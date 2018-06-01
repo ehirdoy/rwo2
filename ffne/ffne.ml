94ed5215fe7748c9908ed45c02b427c3e9225217
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2018-06-01 16:29:29 +0300

    fix ffne

--- ffne/ffne.ml
+++ ffne/ffne.ml
@@ -2,12 +2,13 @@ let find_first_negative_entry array =
   let pos = ref 0 in
   while
     let pos_is_good = !pos < Array.length array in
-    let element_is_non_negative = array.(!pos) >= 0 in
-    pos_is_good && element_is_non_negative
+    let element_is_non_negative idx = array.(!idx) >= 0 in
+    pos_is_good && element_is_non_negative pos
   do
     pos := !pos + 1
   done;
   if !pos = Array.length array then None else Some !pos
 
 
-let test = find_first_negative_entry [|1;2;0;3|]
+let test = find_first_negative_entry [|1;-2;0;3|] = Some 1
+let test = find_first_negative_entry [|1;2;0;3|] = None
